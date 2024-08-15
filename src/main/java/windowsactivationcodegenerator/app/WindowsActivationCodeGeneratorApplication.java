786d11431b70b16c43d6623964ff44dfbed0ae92
hanlie <johansama9999@gmail.com>
2024-08-15 20:46:28 +0700

    [START] Menambahkan Project Management Maven dan Framework Spring dengan dependency Lombok, MySQL Driver, Validation, Spring Data JPA, Spring Web MVC

---
+++ src/main/java/windowsactivationcodegenerator/app/WindowsActivationCodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package windowsactivationcodegenerator.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowsActivationCodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowsActivationCodeGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/windowsactivationcodegenerator/app/WindowsActivationCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package windowsactivationcodegenerator.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowsActivationCodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
